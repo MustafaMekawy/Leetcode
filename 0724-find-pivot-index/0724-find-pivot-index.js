389a85375bdd9902f2923e11f3263e37472db9f2
Mustafa Mekawy <manomostafa29@gmail.com>
2023-08-16 14:50:53 +0300

    Time: 67 ms (73.00%), Space: 44.8 MB (41.98%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+    let sum=0
+    let left =0
+    nums.forEach(num=>{sum+=num})
+    
+    for(let i=0;i<nums.length;i++){
+        let right=sum-left-nums[i]
+        if(left==right) return i
+        left+=nums[i]
+    }
+    return -1
+};
